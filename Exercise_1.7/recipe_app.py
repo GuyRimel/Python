d4c02a88847469653eb3c7eba5fe3be0b49af5a7
GuyRimel <135guyrimel@gmail.com>
2023-06-05 17:55:19 -0500

    working search by ings

--- Exercise_1.7/recipe_app.py
+++ Exercise_1.7/recipe_app.py
@@ -33,24 +33,39 @@ try:
 except:
   print("Problem with: Base.metadata.create_all(engine)")
 
-
 def create_recipe():
   print('======================================================================')
   print('CREATE RECIPE')
   print('======================================================================')
   
-  recipe_name = input('>>> Enter the recipe NAME: ')
-  recipe_cooking_time = None
+  # get the recipe NAME
 
+  recipe_name = ''
+  while not recipe_name:
+    recipe_name = input('>>> Enter the new NAME: ')
+
+    if  len(recipe_name) < 2 or len(recipe_name) > 50:
+      print('### Try again! Name must be between 2 and 50 characters and alphanumeric.')
+      recipe_name = ''
+
+  # get the recipe COOKING TIME
+  recipe_cooking_time = None
   while not recipe_cooking_time:
     try:
       recipe_cooking_time = int(input('>>> Enter the recipe COOK TIME (in minutes): '))
     except:
-      print("### WRONG! Cooking time must be an integer.")
+      print("### WRONG! Cooking time must be a number!")
 
 
+  # get the recipe INGREDIENTS
   # recipe_ingredients is declared as an input string, split by commas, into an array
-  recipe_ingredients = input('>>> List all ingredients (separated by ", "): ').split(', ')
+  recipe_ingredients = []
+  while not recipe_ingredients or recipe_ingredients[0].isnumeric():
+    recipe_ingredients = input('>>> List all ingredients (separated by ", "): ')
+
+  recipe_ingredients = recipe_ingredients.split(', ')
+
+  # calculate recipe difficulty with a cooking time (int) and recipe_ingredients (array)
   recipe_difficulty = calc_difficulty(recipe_cooking_time, recipe_ingredients)
 
   # now that the difficulty is set, recipe_ingredients is joined into recipe_ingredients_string for SQL processing
@@ -67,6 +82,7 @@ def create_recipe():
   session.commit()
 
   print('### Recipe created!')
+  show_main_menu()
 
 
 def update_recipe():
@@ -104,7 +120,11 @@ def update_recipe():
   name_is_changing = input('>>> Want to update the recipe NAME? (y/n): ').lower()
 
   if name_is_changing == 'y':
-    new_name = input('>>> Enter the new NAME: ')
+    new_name = ''
+    while not new_name.isalnum() or len(new_name) < 1 or len(new_name) > 50:
+      new_name = input('>>> Enter the new NAME: ')
+
+    print('### Recipe name has been changed to "' + new_name + '".')
     session.query(Recipe).filter(Recipe.id == recipe_id).update({Recipe.name: new_name})
 
   # update the recipe cooking_time
@@ -112,7 +132,7 @@ def update_recipe():
 
   if ct_is_changing == 'y':
     new_ct = None
-    while not new_ct:
+    while not new_ct or not new_ct.isnumeric():
       try:
         new_ct = int(input('>>> Enter the new recipe COOK TIME (in minutes): '))
         break
@@ -174,31 +194,67 @@ def delete_recipe():
     session.delete(recipe_obj)
     session.commit()
     print('### Recipe deleted!')
+    return show_main_menu()
   else:
     print('### Fear not. Nothing was deleted.')
+    return None
 
 
 
 def search_recipes_by_ingredients():
   print('======================================================================')
   print('SEARCH RECIPES BY INGREDIENTS')
+  if session.query(Recipe).count() < 1:
+    print("### There are no recipes to search yet! - going back to main menu")
+    return None
+  
+  unfiltered_ingredients = []
+
+  results = session.query(Recipe.ingredients).all()
+
+  for recipe_ingredients_list in results:
+    for recipe_ingredients in recipe_ingredients_list:
+      recipe_ingredient_split = recipe_ingredients.split(", ")
+      unfiltered_ingredients.extend(recipe_ingredient_split)
+
+  all_ingredients = []
+  for ing in unfiltered_ingredients:
+    if ing not in all_ingredients:
+      all_ingredients.append(ing)
+
+  print('ALL INGREDIENTS:')
+  print('----------------------------------------------------------------------')
+  for e, ing in enumerate(all_ingredients):
+    print(str(e) + '.) ' + ing)
+
+  ings_searched = ''
+  while not ings_searched:
+    ings_searched = input('Input one or more ingredient numbers to search (separated by a space): ')
+  
+  try:
+    print('you searched for: ', all_ingredients[ings_searched])
+  except:
+    print('Something went wrong!')
+  
 
 
 def show_main_menu():
-  print('======================================================================')
-  print('BEST RECIPES EVER! (CLI): MAIN MENU')
-  print('======================================================================')
-  print('1. Create a new recipe ("new")')
-  print('2. View all recipes ("all")')
-  print('3. Search for a recipe by ingredients ("search")')
-  print('4. Update a recipe ("update")')
-  print('5. Delete a recipe ("delete")')
-  print('6. Quit ("quit")')
-  print('----------------------------------------------------------------------')
+  def print_menu():
+    print('======================================================================')
+    print('BEST RECIPES EVER! (CLI): MAIN MENU')
+    print('======================================================================')
+    print('1. Create a new recipe ("new")')
+    print('2. View all recipes ("all")')
+    print('3. Search for a recipe by ingredients ("search")')
+    print('4. Update a recipe ("update")')
+    print('5. Delete a recipe ("delete")')
+    print('6. Quit ("quit")')
+    print('----------------------------------------------------------------------')
   
   user_selection = None
 
   while user_selection is not '6' or user_selection is not 'quit':
+    print_menu()
     user_selection = input('>>> Enter a number or command: ').lower()
 
     if user_selection == '1' or user_selection == 'new':
